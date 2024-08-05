be61a9d3425ba4d006dd3bbf11538c024a027dc3
Swapnil-2502 <hajareswapnil.2502@gmail.com>
2024-08-05 16:22:28 +0530

    Zod validation

--- backend/types.js
+++ backend/types.js
@@ -1,1 +1,16 @@
+/* eslint-disable no-undef */
 const zod = require("zod")
+
+const createTodo = zod.object({
+    title: zod.string(),
+    description: zod.string()
+})
+
+const updateTodo = zod.object({
+    id: zod.string(),
+})
+
+module.exports = {
+    createTodo,
+    updateTodo
+}
