7797a78ec7e4b68ebadc612f0d7d325d68330eb7
Swapnil-2502 <hajareswapnil.2502@gmail.com>
2024-08-05 17:42:26 +0530

    changes in url

--- backend/db.js
+++ backend/db.js
@@ -9,7 +9,14 @@ require('dotenv').config();
 
 const mongoURI = process.env.MONGODB_URI;
 
-mongoose.connect(mongoURI)
+mongoose.connect(mongoURI,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }).then(() => {
+    console.log('Connected to MongoDB');
+  }).catch((err) => {
+    console.error('Error connecting to MongoDB:', err);
+  })
 
 const TodoSchema = mongoose.Schema({
     title: String,
